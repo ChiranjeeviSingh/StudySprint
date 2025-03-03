df3875b312df7956a2b768297bed9b30ebf7c56f
RAHUL SAI <rahulsai@RAHULs-MacBook-Pro.local>
2025-03-03 01:38:24 -0500

    Added Chnages in Tests

--- frontend/src/App.spec.jsx
+++ frontend/src/App.spec.jsx
@@ -1,9 +1,15 @@
-import {render} from '@testing-library/react';
-import App from './App'
-import React from 'react';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
 
-describe('Renders App',()=>{
-    it('Renders App',()=>{
-        render(<App/>)
-    })
-})
+test("Render App", () => {
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
+
+  const hireEasyText = screen.getByText(/HireEasy/i);
+  expect(hireEasyText).toBeInTheDocument();
+});
